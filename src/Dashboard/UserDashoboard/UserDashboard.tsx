643fa2d963a38e06076446efb7c642721f3fd61f
mahmoodiftee <mahmoodiftee@gmail.com>
2024-12-20 05:23:58 +0600

    added top menu pannel

--- src/Dashboard/UserDashoboard/UserDashboard.tsx
+++ src/Dashboard/UserDashoboard/UserDashboard.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { UserOutlined } from "@ant-design/icons";
+import { LeftOutlined, RightOutlined, UserOutlined } from "@ant-design/icons";
 import { Button, Layout, Menu, theme } from "antd";
 import { Link, Outlet, useNavigate } from "react-router-dom";
 import { useAppDispatch } from "../../Redux/hooks/hooks";
@@ -12,6 +12,7 @@ const siderStyle: React.CSSProperties = {
   overflow: "auto",
   height: "100vh",
   position: "fixed",
+  zIndex: 15,
   insetInlineStart: 0,
   top: 0,
   bottom: 0,
@@ -27,10 +28,21 @@ const AdminDashboard: React.FC = () => {
   const [collapsed, setCollapsed] = useState(false);
   const LogOutDispatch = useAppDispatch();
   const navigate = useNavigate();
+  const menuRef = React.useRef<HTMLDivElement>(null);
 
-  const {
-    token: { colorBgContainer },
-  } = theme.useToken();
+  // Function to scroll left
+  const scrollLeft = () => {
+    if (menuRef.current) {
+      menuRef.current.scrollBy({ left: -200, behavior: "smooth" });
+    }
+  };
+
+  // Function to scroll right
+  const scrollRight = () => {
+    if (menuRef.current) {
+      menuRef.current.scrollBy({ left: 200, behavior: "smooth" });
+    }
+  };
 
   const toggleCollapse = () => {
     setCollapsed(!collapsed);
@@ -98,21 +110,49 @@ const AdminDashboard: React.FC = () => {
           ]}
         />
       </Sider>
-      <Layout style={{ marginInlineStart: collapsed? 60 : 200 }}>
+      <Layout
+        style={{ marginInlineStart: 60, marginTop: 70 }}
+      >
         <Header
-          style={{
-            padding: 0,
-            background: colorBgContainer,
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "space-between",
-            borderBottom: "1px solid #e0e0e0",
-          }}
-        ></Header>
+          style={{ paddingLeft: 0 }}
+          className="fixed top-0 w-full z-10 border-b-[1px] bg-[#f0f2f5] flex justify-center items-center overflow-hidden"
+        >
+          <div className="max-w-maxWidth mx-auto flex justify-center items-center gap-2 gradient-mask ">
+            <div className="flex items-center gap-2">
+              <Button
+                className="hidden md:flex"
+                icon={<LeftOutlined />}
+                onClick={scrollLeft}
+              />
+              <Button
+                className="hidden md:flex"
+                icon={<RightOutlined />}
+                onClick={scrollRight}
+              />
+            </div>
+
+            <div
+              ref={menuRef}
+              className="flex overflow-x-auto items-center no-scrollbar gap-4"
+              style={{
+                flexGrow: 1,
+                whiteSpace: "nowrap",
+                scrollBehavior: "smooth",
+              }}
+            >
+              {[...Array(25).keys()].map((item) => (
+                <Button key={item} color="default" variant="solid">
+                  menu item {item + 1}
+                </Button>
+              ))}
+            </div>
+          </div>
+        </Header>
         <Content
-        className="max-w-maxWidth mx-auto"
+          className={`max-w-maxWidth mx-auto ${
+            collapsed ? "p-1" : "p-1 md:p-6"
+          }`}
           style={{
-            padding: 24,
             minHeight: "100vh",
           }}
         >
